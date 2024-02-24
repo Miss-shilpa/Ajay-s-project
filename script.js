6ad0263600a8db799c8516b4f54840eac43d2496
Shilpa Mishra <126054333+Miss-shilpa@users.noreply.github.com>
2024-02-24 13:33:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+ 
+ var a = 0;
+
+ document.getElementById("count").innerHTML = a;
+
+ var first =document.querySelector("#first");
+ var sec =document.querySelector("#sec");
+
+first.addEventListener("click", function(){ 
+     a = a + 1;
+    document.getElementById("count").innerHTML = a;
+})
+sec.addEventListener("click", function(){
+    a = a - 1;
+    document.getElementById("count").innerHTML = a;    
+
+})
